b9fc16cc64b3b0d39f65eeeca020efdd253dc3fe
nicolas <nicolas.brissonneau@gadz.org>
2017-11-21 17:44:58 -0600

    added z filter

--- handle_tracking/src/object_scan.cpp
+++ handle_tracking/src/object_scan.cpp
@@ -37,6 +37,7 @@ handle_tracking::objectfinder::Response &res)
       {
         ROS_INFO("marker received");
         set_marker(srv.response.handle_marker);
+        marker_sorting(marker_update);
         found+=1;
         std::cout<<found<<std::endl;
       }
@@ -56,7 +57,6 @@ sub.shutdown();
 void Handle_manager::set_marker(const visualization_msgs::MarkerArray markersrv)
 {
     marker_update = markersrv;
-    marker_sorting(marker_update);
 }
 
 void Handle_manager::Publish_visualized_marker(const geometry_msgs::PoseStamped Pose)
@@ -98,38 +98,39 @@ void Handle_manager::Publish_visualized_marker(const geometry_msgs::PoseStamped 
 
 void Handle_manager::marker_sorting(const visualization_msgs::MarkerArray msg)
 {
-    ROS_INFO("sorting started");
-    ROS_INFO("%d", marker_update.markers.size());
-    for (int i = 0 ; i < marker_update.markers.size(); i++)
-    {
-      if ( (x_left > marker_update.markers[i].pose.position.x) /*&& abs(msg->markers[i].pose.position.y)< 0.2*/)
-        { 
-        x_left = marker_update.markers[i].pose.position.x;
-        grasp_pose.pose=marker_update.markers[i].pose;
-        grasp_pose.header =marker_update.markers[i].header;
-        ROS_INFO("New minimum value found : %f", x_left);
-        }
-    }
+  ROS_INFO("%d", marker_update.markers.size());
+  for (int i = 0 ; i < marker_update.markers.size(); i++)
+  {
 
     grasp_pose.header.stamp = ros::Time::now();
     grasp_pose.header.frame_id = "head_rgbd_sensor_rgb_frame";
-
+    grasp_pose.pose = marker_update.markers[i].pose;
+    grasp_pose.header = marker_update.markers[i].header;
     //transform
     tf::StampedTransform transform_sensor_base;
     listener.waitForTransform("head_rgbd_sensor_rgb_frame","odom",  ros::Time(0), ros::Duration(3.0));
-    while (ros::ok()){
-    try{ 
+    int ite = 0;
+    while (ros::ok() && ite < 20)
+    {
+      try
+      { 
         listener.lookupTransform("head_rgbd_sensor_rgb_frame","odom", ros::Time(0), transform_sensor_base);
         listener.transformPose (std::string("odom"), grasp_pose,  grasp_transformed_pose) ; 
-        ROS_INFO("_x : %.3lf, _y : %.3lf, _z : %.3lf \n ", grasp_transformed_pose.pose.position.x, grasp_transformed_pose.pose.position.y, grasp_transformed_pose.pose.position.z) ; 
-        break;
-    }   
-    catch (tf::TransformException &ex){
+        grasp_transformed_pose.header.stamp=ros::Time::now();
+        grasp_transformed_pose.header.frame_id="odom";
+        if ( (x_left > grasp_transformed_pose.pose.position.x) && (fabs(grasp_transformed_pose.pose.position.z-0.94)<0.05) )
+        { 
+          ROS_INFO("_x : %.3lf, _y : %.3lf, _z : %.3lf \n ", grasp_transformed_pose.pose.position.x, grasp_transformed_pose.pose.position.y, grasp_transformed_pose.pose.position.z) ; 
+          Publish_visualized_marker(grasp_transformed_pose);
+          x_left = grasp_transformed_pose.pose.position.x;
+          break;
+        }
+      }   
+      catch (tf::TransformException &ex){
+      }
+      ite = ite +1;
     }
-    }
-    grasp_transformed_pose.header.stamp=ros::Time::now();
-    grasp_transformed_pose.header.frame_id="odom";
-    Publish_visualized_marker(grasp_transformed_pose);
-/*    grasp_pub.publish(grasp_transformed_pose);
-*/
+            std::cout <<grasp_transformed_pose.pose.position.z << " aaaaaaand " << fabs(grasp_transformed_pose.pose.position.z-0.94)<<std::endl;
+
+  }
 }
